89a25e559be1aa8252e58cb4ca6e329837318a54
Lova Widmark <lova@avoidpointer.com>
2023-01-02 13:35:30 +0100

    Update set-env.bat

--- set-env.bat
+++ set-env.bat
@@ -190,7 +190,7 @@ set LLVM_CMAKE_CONFIGURE_FLAGS= ^
 	-DLLVM_INCLUDE_TESTS=OFF ^
 	-DLLVM_INCLUDE_UTILS=OFF ^
 	-DLLVM_TEMPORARILY_ALLOW_OLD_TOOLCHAIN=ON ^
-	-DLLVM_ENABLE_PROJECTS=lld ^
+	-DLLVM_ENABLE_PROJECTS=lld;clang ^
 	%LLVM_CMAKE_CONFIGURE_EXTRA_FLAGS%
 
 :: . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
